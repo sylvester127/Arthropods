b50821dd1b303a1ff75e0e033eb1a25f6469af3a
naram Kim <sylvester127@naver.com>
2019-11-10 01:35:42 +0900

    refactor: Change ArthropodInfo to ScientificName and Habitat

---
+++ AMS/app/src/main/java/com/sylvester/ams/model/Habitat.java
@@ -0,0 +1,71 @@
+package com.sylvester.ams.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Habitat extends RealmObject {
+    @PrimaryKey
+    private int id;   // 아이디
+    private String continentType;   // 뉴월드 or 올드월드
+    private String habitatName;     // 서식지명
+    private String environment;     // 서식환경
+    private String temperature;     // 온도
+    private String humidity;        // 습도
+
+    public Habitat(int id, String continentType, String habitatName, String environment, String temperature, String humidity) {
+        this.id = id;
+        this.continentType = continentType;
+        this.habitatName = habitatName;
+        this.environment = environment;
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContinentType() {
+        return continentType;
+    }
+
+    public void setContinentType(String continentType) {
+        this.continentType = continentType;
+    }
+
+    public String getHabitatName() {
+        return habitatName;
+    }
+
+    public void setHabitatName(String habitatName) {
+        this.habitatName = habitatName;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+}
---
+++ AMS/app/src/main/java/com/sylvester/ams/model/ScientificName.java
@@ -0,0 +1,76 @@
+package com.sylvester.ams.model;
+
+import java.util.ArrayList;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class ScientificName extends RealmObject{
+    @PrimaryKey
+    private int id;                             // 학명 아이디(PK)
+
+    private String genus;                       // 종명
+    private String species;                     // 속명
+    private String commonName;                  // 커먼네임
+    private ArrayList<Arthropod> arthropods;    // 관리하는 절지동물 목록
+    private Habitat habitat;                    // 서식지 정보
+
+    // 생성자
+    public ScientificName(int id, String genus, String species, String commonName,ArrayList<Arthropod> arthropods, Habitat habitat) {
+        this.id = id;
+        this.genus = genus;
+        this.species = species;
+        this.commonName = commonName;
+        this.arthropods = arthropods;
+        this.habitat = habitat;
+    }
+
+    // getter and setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public ArrayList<Arthropod> getArthropods() {
+        return arthropods;
+    }
+
+    public void setArthropods(ArrayList<Arthropod> arthropods) {
+        this.arthropods = arthropods;
+    }
+
+    public Habitat getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(Habitat habitat) {
+        this.habitat = habitat;
+    }
+}
